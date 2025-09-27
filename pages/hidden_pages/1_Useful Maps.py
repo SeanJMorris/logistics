ac5915a64648f6f32e861dbb81b6e2d3307b79ba
Sean Morris <seanj.morris@gmail.com>
2025-09-27 15:56:35 -0400

    Reconfigure test pages and real pages after making mobile-sensitive version go active.

---
+++ pages/hidden_pages/1_Useful Maps.py
@@ -0,0 +1,24 @@
+import streamlit as st # type: ignore
+from streamlit_image_select import image_select # type: ignore
+
+st.set_page_config(
+    page_title = "Useful Maps",
+    layout="wide",
+    page_icon="🗺️"
+)
+
+st.title("Useful Maps")
+
+images_and_captions = {
+    "photos/York And Ogunquit Driving Map.png": "Weekend Location Overview",
+    "photos/Downtown Ogunquit.png": "Downtown Ogunquit & Parking Map",
+    "photos/York Harbor Inn & Hartley Mason Reserve.png": "York Harbor Inn & Hartley Mason Reserve Map",
+    "photos/Ceremony Seating Chart & Procession Order.png": "Ceremony Ceating Chart & Procession Order",
+    "photos/York Harbor Inn Main Building.png": "York Harbor Inn Main Building",
+    "photos/Seacoast Pickleball - Farewell Brunch.png": "Seacoast Pickleball"
+}
+
+image_selector = image_select("", list(images_and_captions.keys()))
+
+st.subheader(images_and_captions[image_selector])
+st.image(image_selector)
