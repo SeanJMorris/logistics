7924d63e04678d518836154e10ff02e24f81e748
Sean Morris <seanj.morris@gmail.com>
2025-10-20 21:37:53 -0400

    Comment out ceremony seating chart image in Useful Guides page.

--- pages/1_Useful Guides.py
+++ pages/1_Useful Guides.py
@@ -14,7 +14,7 @@ def mobile():
     st.image("photos/York And Ogunquit Driving Map.png", caption="Weekend Location Overview")
     st.image("photos/Downtown Ogunquit.png", caption="Downtown Ogunquit & Parking Map - Search for these lots on Google Maps.")
     st.image("photos/York Harbor Inn & Hartley Mason Reserve.png", caption="York Harbor Inn & Hartley Mason Reserve Map")
-    st.image("photos/Ceremony Seating Chart & Procession Order.png", caption="Ceremony Seating Chart & Procession Order")
+    # st.image("photos/Ceremony Seating Chart & Procession Order.png", caption="Ceremony Seating Chart & Procession Order")
     st.image("photos/Ceremony Microphones and Camera Logistics.png", caption="Ceremony Microphones and Camera Logistics")
     st.image("photos/York Harbor Inn Main Building.png", caption="York Harbor Inn Main Building")
     st.image("photos/Seacoast Pickleball - Farewell Brunch.png", caption="Seacoast Pickleball")
@@ -24,7 +24,7 @@ def pc():
         "photos/York And Ogunquit Driving Map.png": "Weekend Location Overview",
         "photos/Downtown Ogunquit.png": "Downtown Ogunquit & Parking Map",
         "photos/York Harbor Inn & Hartley Mason Reserve.png": "York Harbor Inn & Hartley Mason Reserve Map",
-        "photos/Ceremony Seating Chart & Procession Order.png": "Ceremony Seating Chart & Procession Order",
+        # "photos/Ceremony Seating Chart & Procession Order.png": "Ceremony Seating Chart & Procession Order",
         "photos/Ceremony Microphones and Camera Logistics.png": "Ceremony Microphones and Camera Logistics",
         "photos/York Harbor Inn Main Building.png": "York Harbor Inn Main Building",
         "photos/Seacoast Pickleball - Farewell Brunch.png": "Seacoast Pickleball"
